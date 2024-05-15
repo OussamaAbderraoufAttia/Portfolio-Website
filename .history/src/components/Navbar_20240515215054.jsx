8cb72e9b9261ae8e751762148a40128491d9e789
Oussama Abderraouf Attia <92870205+OussamaAbderraoufAttia@users.noreply.github.com>
2024-05-15 21:51:07 +0100

    final

---
+++ .history/src/components/Navbar_20240515215054.jsx
@@ -0,0 +1,96 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { navLinks } from "../constants";
+import OussamaImage from '../images/Oussama.jpg';
+import closeIcon from '../images/x.svg';
+import menuIcon from '../images/menu.svg';
+
+const Navbar = () => {
+  const [active, setActive] = useState("");
+  const [toggle, setToggle] = useState(false);
+
+  const toggleResume = () => {
+    const resumeUrl = "https://drive.google.com/file/d/1WeHR-sPqeZHGi3y1qQy-EFfdDWj6-o8W/view";
+    window.open(resumeUrl);
+  };
+
+  const toggleLinkTree = () => {
+    const linkTree = "https://linktr.ee/oussamaabderraoufattia";
+    window.open(linkTree);
+  };
+
+  const renderNavLinks = (isSecondary) => (
+    <ul className={`list-none ${isSecondary ? "flex sm:hidden" : "hidden sm:flex"} flex-row gap-6`}>
+      {navLinks.map((link) => (
+        <li
+          key={link.id}
+          className={`${
+            active === link.title && !isSecondary ? "text-white" : "text-white"
+          } hover:text-good-purple text-sm font-medium cursor-pointer`}
+          onClick={() => {
+            setActive(link.title);
+            if (isSecondary) {
+              setToggle(false);
+            }
+          }}
+        >
+          <a href={`#${link.id}`}>{link.title}</a>
+        </li>
+      ))}
+      <li className="text-white hover:text-good-purple text-sm font-medium cursor-pointer">
+        <button onClick={toggleResume}>Resume</button>
+      </li>
+      <li className="text-white hover:text-good-purple text-sm font-medium cursor-pointer">
+        <button onClick={toggleLinkTree}>Find me online!</button>
+      </li>
+    </ul>
+  );
+
+  return (
+    <>
+      <nav className="w-full flex items-center py-3 fixed top-0 z-20"> {/* Removed bg-primary */}
+        <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+          <Link
+            to="/"
+            className="flex items-center gap-2"
+            onClick={() => {
+              setActive("");
+              window.scrollTo(0, 0);
+            }}
+          >
+            <img src={OussamaImage} alt="logo" className="w-9 h-9 ml-2 object-contain rounded-full" />
+            <p className="text-white text-lg font-bold cursor-pointer flex">
+              Oussama Abderraouf&nbsp;
+              <span className="sm:block hidden">ATTIA</span>
+            </p>
+          </Link>
+          {renderNavLinks(false)}
+          <div className="sm:hidden flex flex-1 justify-end items-center">
+            {/* Toggle button */}
+            {toggle ? (
+              <img
+                src={closeIcon}
+                alt="close"
+                className="w-6 h-6 mr-2 object-contain cursor-pointer"
+                onClick={() => setToggle(false)}
+              />
+            ) : (
+              <img
+                src={menuIcon}
+                alt="menu"
+                className="w-6 h-6 mr-2 object-contain cursor-pointer"
+                onClick={() => setToggle(true)}
+              />
+            )}
+            {/* Condensed navbar links */}
+            <div className={`p-4 absolute top-14 right-0 mx-2 my-2 min-w-[120px] z-10 ${toggle ? "flex bg-black bg-opacity-75" : "hidden"}`}>
+              {renderNavLinks(true)}
+            </div>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
---
+++ .history/src/components/canvas/Computers_20240515215053.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, useGLTF } from "@react-three/drei";
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
+
+const ComputerModel = () => {
+  const { scene } = useGLTF(
+    "./desktop_pc/scene.gltf",
+    undefined,
+    (loader) => {
+      const dracoLoader = new DRACOLoader();
+      loader.setDRACOLoader(dracoLoader);
+    }
+  );
+
+  return (
+    <group>
+      <mesh>
+        <hemisphereLight intensity={0.15} groundColor="black" />
+        <spotLight
+          position={[-20, 50, 10]}
+          angle={0.12}
+          penumbra={1}
+          intensity={1}
+          castShadow
+          shadow-mapSize={1024}
+        />
+        <pointLight intensity={1} />
+        <primitive
+          object={scene}
+          scale={0.75}
+          position={[0, -3.25, -1.5]}
+          rotation={[-0.01, -0.2, -0.1]}
+        />
+      </mesh>
+    </group>
+  );
+};
+
+const ComputersCanvas = () => {
+  return (
+    <div style={{ width: "100%", height: "100%" }}>
+      <Canvas
+        frameloop="demand"
+        shadows
+        dpr={[1, 2]}
+        camera={{ position: [20, 3, 5], fov: 25 }}
+        gl={{ preserveDrawingBuffer: true }}
+      >
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <ComputerModel />
+      </Canvas>
+    </div>
+  );
+};
+
+export default ComputersCanvas;
